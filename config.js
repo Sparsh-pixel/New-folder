178f348830d22a3f9f4147a1c98033f2d796d5f3
Sparsh-pixel <sparshprasad7@gmail.com>
2024-10-13 12:59:02 +0530

    first commit

---
+++ config.js
@@ -0,0 +1,20 @@
+tailwind.config = {
+  theme: {
+    extend: {
+      colors: {
+        pale_orange: "#ffd9a6",
+        light_orange: "#fbb03b",
+        orange: "#f7931e",
+      },
+      fontFamily: {
+        heading: ["Playfair Display SC"],
+        jose: ["Josefin Sans"],
+        pop: ["poppins"],
+      },
+      backgroundImage: {
+        mobile:"url('bg-mobile.jpg')",
+        desktop:"url('bg-desktop.png')",
+      }
+    },
+  },
+};
